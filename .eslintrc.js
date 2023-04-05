020fdd2fbd93e992c933ad440bc4dac2d00b6927
Hoang Nguyen Xuan <xhoang0509@gmail.com>
2023-04-05 23:01:02 +0700

    edit auth middleware

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,38 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true,
-        "node": true
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
+        node: true,
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
+    extends: 'eslint:recommended',
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
     },
-    "plugins": [
-        "spellcheck"
-    ],
-    "rules": {
-        "spellcheck/spell-checker": [2, {
-            "strings": true,
-            "comments": false,
-            "identifiers": true,
-            "templates": true,
-            "lang": "en_US",
-            "skipWords": [
-                "dotenv",
-                "req",
-                "res",
-                "jsonwebtoken",
-                "Xuan",
-                "Hoang",
-                "Datphong",
-                "nodemailer",
-                "gmail",
-                "bqolkurhokehdqia"
-            ]
-        }]
-    }
-
-}
+    plugins: ['spellcheck'],
+    rules: {
+        'spellcheck/spell-checker': [
+            2,
+            {
+                strings: true,
+                comments: false,
+                identifiers: true,
+                templates: true,
+                lang: 'en_US',
+                skipWords: [
+                    'dotenv',
+                    'req',
+                    'res',
+                    'jsonwebtoken',
+                    'Xuan',
+                    'Hoang',
+                    'Datphong',
+                    'nodemailer',
+                    'gmail',
+                    'bqolkurhokehdqia',
+                ],
+            },
+        ],
+    },
+};
--- src/middlewares/requiresAuth.middleware.js
+++ src/middlewares/requiresAuth.middleware.js
@@ -1,18 +1,43 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const writeLog = require('../logger');
+const admins = require('../models').admins;
+const users = require('../models').users;
 
-function authApi(req, res, next) {
+async function authApi(req, res, next) {
     try {
         const token = req.headers.authorization.replace(/Bearer /g, '');
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY, {
             algorithms: process.env.JWT_ALGORITHM,
             ignoreExpiration: true,
         });
-        // console.log('decoded: ', decoded);
-        // console.log('decoded.email: ', decoded.email);
-        writeLog(__filename, 'requiresAuth.authApi', 'OK');
-        next();
+        console.log('decoded: ', decoded);
+        const now = Math.floor(Date.now() / 1000);
+
+        if (decoded.exp < now) {
+            writeLog(__filename, 'requiresAuth.authApi', 'JWT has expired');
+            res.status(401).json({ message: 'JWT expired' });
+        } else {
+            if (decoded.type === 'admin') {
+                const admin = await admins.findOne({ where: { id: decoded.id } });
+                if (admin) {
+                    writeLog(__filename, 'requiresAuth.authApi', 'OK');
+                    next();
+                } else {
+                    writeLog(__filename, 'requiresAuth.authApi', 'Admin not found');
+                    res.status(401).json({ message: 'Admin not found' });
+                }
+            } else if (decoded.type === 'user') {
+                const user = await users.findOne({ where: { id: decoded.id } });
+                if (user) {
+                    writeLog(__filename, 'requiresAuth.authApi', 'OK');
+                    next();
+                } else {
+                    writeLog(__filename, 'requiresAuth.authApi', 'User not found');
+                    res.status(401).json({ message: 'User not found' });
+                }
+            }
+        }
     } catch (e) {
         writeLog(__filename, 'requiresAuth.authApi', `ERROR:  ${JSON.stringify(e)}`);
         res.status(401).json({ message: 'UNAUTHORIZED' });
